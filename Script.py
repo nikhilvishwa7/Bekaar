9d120c770f5d6cf35abd0edb103048ac530cc5d7
1238muj <122881477+1238muj@users.noreply.github.com>
2023-05-13 14:58:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -207,7 +207,7 @@ IMDb Data:
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10</b>"""
- href='https://t.me/HOWTODOWNLOADRIO/378'>⚡HOW TODOWNLOAD⚡</a>  
+  
     ALL_FILTERS = """
 <b>Hᴇʏ {}, Tʜᴇsᴇ ᴀʀᴇ ᴍʏ ᴛʜʀᴇᴇ ᴛʏᴘᴇs ᴏғ ғɪʟᴛᴇʀs.</b>"""
     
